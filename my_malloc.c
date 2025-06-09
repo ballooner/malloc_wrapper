cb4342c52622267505ef30a9140f494557e7f599
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-06-09 17:00:43 -0400

    Added memory block size storage before actual block to assist with realloc

--- my_malloc.c
+++ my_malloc.c
@@ -7,9 +7,16 @@
 
 void* my_malloc_debug(size_t size, char *calledFrom, int line)
 {
+    size_t totalSize = size + sizeof(size_t);
+
     void *mem;
-    if ((mem = malloc(size)) == NULL)
+    if ((mem = malloc(totalSize)) == NULL)
 	exit(1);
+
+    //Store the size of block
+    *((size_t*)mem) = size;
+    //Move pointer to after metadata
+    mem = (void*)((char*)mem + sizeof(size_t));
 
     printf("[malloc] %zu bytes at %p from %s:%d\n", size, mem, calledFrom, line);
 
@@ -22,10 +29,15 @@ void* my_calloc_debug(size_t n, size_t size, char *calledFrom, int line)
     if (n == 0 || size == 0)
 	return NULL;
 
-    size_t mem_size = n * size;
-    void *mem = malloc(mem_size);
+    size_t mem_size = (n * size) + sizeof(size_t);
+    void *mem;
+    if ((mem = malloc(mem_size)) == NULL)
+	exit(1);
 
-    memset(mem, 0, mem_size);
+    *((size_t*)mem) = size * n;
+    mem = (void*)((char*)mem + sizeof(size_t));
+
+    memset(mem, 0, mem_size - sizeof(size_t));
 
     printf("[calloc] %zu elements of %zu bytes allocated at %p and zeroed from %s:%d\n",
 	    n, size, mem, calledFrom, line);
@@ -40,6 +52,9 @@ void* my_realloc_debug(void *mem, size_t size, char *calledFrom, int line)
 
 void my_free_debug(void *mem, char *calledFrom, int line)
 {
-    printf("[free] %p from %s:%d\n", mem, calledFrom, line);
+    size_t size = *((char*)mem - sizeof(size_t));
+    mem = (void*)((char*)mem - sizeof(size_t));
+
+    printf("[free] %zu bytes at %p from %s:%d\n", size, mem, calledFrom, line);
     free(mem);
 }
